5d125deddbe6218cc596bc19443ceb895cf4504b
rogee <rogeecn@gmail.com>
2017-12-13 14:41:55 +0800

    add payments

--- console/migrations/m171212_093335_init_webconfig_data.php
+++ console/migrations/m171212_093335_init_webconfig_data.php
@@ -86,11 +86,11 @@ class m171212_093335_init_webconfig_data extends Migration
             [4, 'storage.aliyun_oss.network', "阿里云OSS 网络类型", "classic", 'single', 0, json_encode(['classic' => '经典网络', 'vpc' => 'VPC']), time()],
             [4, 'storage.aliyun_oss.is_internal', "阿里云OSS 内网模式", "0", 'single', 0, json_encode(['是', '否']), time()],
             [4, 'storage.aliyun_oss.bucket', "阿里云OSS Bucket", "", 'string', 0, "", time()],
-            [5, 'wechat.token', "Token", "", 'string', 0, "", time()],
-            [6, 'payment.alipay.token', "支付宝", "", 'string', 0, "", time()],
-            [6, 'payment.wechatpay.token', "微信支付", "", 'string', 0, "", time()],
             [5, 'wechat.app_id', "AppID", "", 'string', 0, "", time()],
             [5, 'wechat.app_secret', "AppSecret", "", 'string', 0, "", time()],
+            [6, 'payment.channel', "支付通道", "", 'multiple', 0, json_encode($payment), time()],
+            [6, 'payment.alipay.token', "支付宝", "", 'string', 0, "", time()],
+            [6, 'payment.wechatpay.token', "微信支付", "", 'string', 0, "", time()],
         ]);
     }
 
