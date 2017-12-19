0ec9e643c02b7862b64957654dee49541bde563a
rogee <rogeecn@gmail.com>
2017-12-19 23:19:42 +0800

    add UEditor

--- console/migrations/m171219_032200_alter_doctor_service_time.php
+++ console/migrations/m171219_032200_alter_doctor_service_time.php
@@ -13,6 +13,7 @@ class m171219_032200_alter_doctor_service_time extends Migration
         $this->dropColumn($this->_table, "max_time_long");
         $this->dropColumn($this->_table, "ticket_count");
         $this->dropColumn($this->_table, "week_service_start_at");
+        $this->dropColumn($this->_table, "price");
     }
 
     public function safeUp()
@@ -22,5 +23,6 @@ class m171219_032200_alter_doctor_service_time extends Migration
         $this->addColumn($this->_table, "max_time_long", $this->string()->notNull()->defaultValue(0));
         $this->addColumn($this->_table, "ticket_count", $this->integer()->notNull()->defaultValue(0)->after("doctor_id"));
         $this->addColumn($this->_table, "week_service_start_at", $this->string()->notNull()->defaultValue(0)->after("week"));
+        $this->addColumn($this->_table, "price", $this->integer()->notNull()->defaultValue(0)->after("doctor_id"));
     }
 }
