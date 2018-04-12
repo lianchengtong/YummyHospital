b5f6d21ed8318afc2bbbcd908c44fdb1360f7e01
rogee <rogeecn@gmail.com>
2018-04-12 21:23:58 +0800

    complete

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DEPLOY_PATH=/root/mp.711xd.com
+cd $DEPLOY_PATH
+rm -rf application  backup  common  composer.json  console  environments  init  init.bat  LICENSE  README.md  requirements.php  test.php  tools  yii
+
+cd /home/projects/handian_wechat
+cp -rap application  backup  common  composer.json  console  environments  init  init.bat  LICENSE  README.md  requirements.php  test.php  tools  yii $DEPLOY_PATH
+
+cd $DEPLOY_PATH
+php init --env=Production --overwrite=y
+
+
+echo DONE
